3297264fff27c0044d54b3b369b9c2ee6517b459
92sankor <92sankor@gafe.molndal.se>
2024-09-23 13:58:06 +0200

    swagger

--- src/main/java/sandrakorpi/molnintegrationbookapp/Controllers/BookController.java
+++ src/main/java/sandrakorpi/molnintegrationbookapp/Controllers/BookController.java
@@ -1,5 +1,7 @@
 package sandrakorpi.molnintegrationbookapp.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import sandrakorpi.molnintegrationbookapp.Services.BookService;
 
 import java.util.List;
 
+@Tag(name = "Book", description = "Operationer relaterade till böcker")
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -21,43 +24,45 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Lägg till en ny bok")
     @PostMapping("/add")
     public ResponseEntity<Book> addBook(@Valid @RequestBody BookDto bookDto) {
         Book addedBook = bookService.addBook(bookDto);
         return ResponseEntity.status(HttpStatus.OK).body(addedBook);
     }
+
+    @Operation(summary = "Lägg till en bok till en lista")
     @PostMapping("/addToList")
-    public ResponseEntity<Book> addBookToList (@Valid @RequestBody BookDto bookDto, @RequestParam int userId, @RequestParam boolean isFavorite) {
-        // Lägg till eller uppdatera boken för den angivna användaren och listan (favorit eller att läsa)
+    public ResponseEntity<Book> addBookToList(@Valid @RequestBody BookDto bookDto, @RequestParam int userId, @RequestParam boolean isFavorite) {
         Book addedOrUpdatedBook = bookService.addOrUpdateBookForUser(userId, bookDto, isFavorite);
         return ResponseEntity.status(HttpStatus.OK).body(addedOrUpdatedBook);
     }
 
+    @Operation(summary = "Hämta alla böcker")
     @GetMapping
     public ResponseEntity<List<Book>> getAllBooks() {
         List<Book> bookList = bookService.getAllBooks();
         return ResponseEntity.ok(bookList);
     }
 
+    @Operation(summary = "Hämta bok med specifikt ID")
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable int id) {
         Book searchBook = bookService.getBookById(id);
         return ResponseEntity.ok(searchBook);
-
     }
 
+    @Operation(summary = "Uppdatera en bok")
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable int id, @Valid @RequestBody BookDto bookDto) {
         Book updatedBook = bookService.updateBook(id, bookDto);
         return ResponseEntity.ok().body(updatedBook);
     }
 
+    @Operation(summary = "Ta bort en bok")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable int id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
     }
 }
-
-
-
--- src/main/java/sandrakorpi/molnintegrationbookapp/Controllers/UserController.java
+++ src/main/java/sandrakorpi/molnintegrationbookapp/Controllers/UserController.java
@@ -1,20 +1,21 @@
 package sandrakorpi.molnintegrationbookapp.Controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sandrakorpi.molnintegrationbookapp.DTOs.UserDTO;
 import sandrakorpi.molnintegrationbookapp.Models.User;
-import sandrakorpi.molnintegrationbookapp.Repositories.UserRepository;
 import sandrakorpi.molnintegrationbookapp.Services.UserService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User", description = "Operationer relaterade till användare")
 public class UserController {
     private final UserService userService;
 
@@ -22,15 +23,17 @@ public class UserController {
         this.userService = userService;
     }
 
-
     @GetMapping
+    @Operation(summary = "Hämta alla användare", description = "Hämtar en lista med alla användare.")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.findAllUsers();
         return ResponseEntity.ok(users);
     }
+
     @GetMapping("/{email}")
+    @Operation(summary = "Hämta användare med specifik e-post", description = "Hämtar en användare baserat på den angivna e-postadressen.")
     public ResponseEntity<UserDTO> findUserByEmail(@PathVariable String email) {
         UserDTO foundUser = userService.findUserByEmail(email);
         return ResponseEntity.ok().body(foundUser);
     }
-    }
+}
